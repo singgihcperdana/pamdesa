993d487ddf2daf12c1f75e5f7f4e75e4d204c699
Singgih Perdana <singgihcp88@gmail.com>
2025-01-27 13:21:16 +0700

    add scheduler remove expired token in db

--- src/main/java/org/pamdesa/Main.java
+++ src/main/java/org/pamdesa/Main.java
@@ -2,11 +2,8 @@ package org.pamdesa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableScheduling
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
